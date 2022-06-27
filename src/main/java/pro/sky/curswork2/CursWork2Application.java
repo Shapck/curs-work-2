b6f109dc2bcfcb8d747176bd2829cc31db33b6e1
Дима <dimonsoldatov@gmsil.com>
2022-06-27 15:34:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/curswork2/CursWork2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.curswork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursWork2Application.class, args);
+	}
+
+}
